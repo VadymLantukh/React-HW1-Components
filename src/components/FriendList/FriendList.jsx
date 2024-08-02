528e0dbd44a755b187e8858c78796d210d6199c1
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-08-02 17:06:50 +0300

    Fix css style

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,9 @@
 import FriendListItem from './FriendListItem';
-import css from './FriendList.module.css'
+import css from './FriendList.module.css';
 
-const FriendList = ({friends}) => {
+const FriendList = ({ friends }) => {
   return (
-    <ul className={ css.list}>
+    <ul className={css.list}>
       {friends.map(friend => {
         return (
           <li className={css.item} key={friend.id}>
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -3,8 +3,8 @@ import css from './FriendListItem.module.css';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div className={css.boxFriend}>
-      <img className={ css.img} src={avatar} alt="Avatar" width="48" />
-      <p className={ css.name}>{name}</p>
+      <img className={css.img} src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
       {isOnline ? (
         <p className={css.colorOnline}>Online</p>
       ) : (
